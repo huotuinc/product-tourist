5f2d46d8d20ad21ba7c3bcee69d5abeb6eb87475
lihuaixin <1072931117@qq.com>
2016-12-16 17:51:57 +0800

    entity添加注解

--- tourist-service/src/main/java/com/huotu/tourist/entity/ActivityType.java
+++ tourist-service/src/main/java/com/huotu/tourist/entity/ActivityType.java
@@ -1,14 +1,25 @@
 package com.huotu.tourist.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * 活动类型
  * Created by lhx on 2016/12/16.
  */
-
+@Entity
+@Table(name = "activity_type")
+@Getter
+@Setter
 public class ActivityType extends BaseModel {
 
     /**
      * 活动名称
      */
+    @Column(name = "activityName", length = 200)
     private String activityName;
 }
--- tourist-service/src/main/java/com/huotu/tourist/entity/PresentRecord.java
+++ tourist-service/src/main/java/com/huotu/tourist/entity/PresentRecord.java
@@ -2,6 +2,9 @@ package com.huotu.tourist.entity;
 
 import com.huotu.tourist.common.PresentStateEnum;
 
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.math.BigDecimal;
 
 /**
@@ -14,20 +17,25 @@ public class PresentRecord extends BaseModel{
     /**
      * 供应商
      */
+    @ManyToOne
+    @JoinColumn(name = "touristSupplierId")
     private TouristSupplier touristSupplier;
 
     /**
      * 提现金额
      */
-    private BigDecimal AmountOfMoney;
+    @Column(name = "amountOfMoney")
+    private BigDecimal amountOfMoney;
 
     /**
      * 账户余额
      */
-    private BigDecimal AccountBalance;
+    @Column(name = "accountBalance")
+    private BigDecimal accountBalance;
 
     /**
      * 提现状态
      */
+    @Column(name = "presentState")
     private PresentStateEnum presentState;
 }
