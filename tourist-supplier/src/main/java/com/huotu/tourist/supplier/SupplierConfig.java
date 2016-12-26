5beddf265b78579acdf889c2f23f5976139c00e1
Cai Jiang <luffy.ja@gmail.com>
2016-12-26 15:47:44 +0800

    先行移除这个

--- tourist-supplier/src/main/java/com/huotu/tourist/supplier/SupplierConfig.java
+++ tourist-supplier/src/main/java/com/huotu/tourist/supplier/SupplierConfig.java
@@ -1,12 +1,18 @@
-package com.huotu.tourist.supplier;
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
+ */
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+package com.huotu.tourist.supplier;
 
 /**
  * Created by lhx on 2016/12/16.
  */
-@Configuration
-@ComponentScan("com.huotu.tourist.supplier.controller")
+//@Configuration
+//@ComponentScan("com.huotu.tourist.supplier.controller")
 public class SupplierConfig {
 }
