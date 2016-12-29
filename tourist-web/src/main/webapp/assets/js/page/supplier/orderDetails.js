315579a2ca1abd62ff623cdecb5f2b6e3d072884
shiliting <861819089@qq.com>
2016-12-29 11:57:55 +0800

    订单详情页，脚本添加

--- tourist-web/src/main/webapp/assets/js/page/supplier/orderDetails.js
+++ tourist-web/src/main/webapp/assets/js/page/supplier/orderDetails.js
@@ -1,46 +1,64 @@
 /**
- * Created by Administrator xhl 2015/12/23.
+ * Created by Administrator slt 2016/12/29
  */
-define(function (require, exports, module) {
-    $("#addModelForm").validate({
-        rules: {
-            txtModelName: {
-                required: true,
-            },
-            txtModelDescription: {
-                maxlength: 200
-            },
-            txtModelType: {
-                selrequired: "-1"
-            },
-            txtOrderWeight: {
-                digits: true,
-            }
-        },
-        messages: {
-            txtModelName: {
-                required: "模型名称为必输项"
-            },
-            txtModelDescription: {
-                maxlength: "模型描述不能超过200个字符"
-            },
-            txtModelType: {
-                selrequired: "请选择模型类型"
-            },
-            txtOrderWeight: {
-                digits: "请输入数字",
-            }
-        },
-        submitHandler: function (form, ev) {
-            var commonUtil = require("common");
-            commonUtil.setDisabled("jq-cms-Save");
-            var layer = require("layer");
-            layer.msg("操作成功", {time: 2000});
-            commonUtil.cancelDisabled("jq-cms-Save");
-            return false;
-        },
-        invalidHandler: function () {
-            return true;
+$(function(){
+    checksOptimize();
+});
+
+var checksOptimize=function(){
+    $("input[type='radio']").iCheck({
+        checkboxClass: "icheckbox_square-green",
+        radioClass: "iradio_square-green"
+    })
+
+};
+
+var modifyTravelerInfo=function(obj){
+    var tds=$(obj).parent().parent().children("td");
+    var id=$(tds[0]).text();
+    var name=$(tds[1]).text();
+    var sex=$(tds[2]).text()=="男"?0:1;
+    var age=$(tds[3]).text();
+    var tel=$(tds[4]).text();
+    var IDNo=$(tds[5]).text();
+    $("input[name='name']","#modifyTravelerInfo").val(name);
+    $("input[name='sex'][value='"+sex+"']","#modifyTravelerInfo").iCheck('check');
+    $("input[name='age']","#modifyTravelerInfo").val(age);
+    $("input[name='tel']","#modifyTravelerInfo").val(tel);
+    $("input[name='IDNo']","#modifyTravelerInfo").val(IDNo);
+
+
+
+    layer.open({
+        type:1,
+        title:"修改人员信息",
+        btn:["确定"],
+        area:["600px","360px"],
+        content:$("#modifyTravelerInfo"),
+        yes:function(index){
+            name=$("input[name='name']","#modifyTravelerInfo").val();
+            sex=$("input[name='sex']:checked","#modifyTravelerInfo").val();
+            age=$("input[name='age']","#modifyTravelerInfo").val();
+            tel=$("input[name='tel']","#modifyTravelerInfo").val();
+            IDNo=$("input[name='IDNo']","#modifyTravelerInfo").val();
+            $.ajax({
+                type:'POST',
+                url: '../../mock/supplier/orderDetails.json',
+                dataType: 'json',
+                data: {id:id,name:name,sex:sex,age:age,tel:tel,IDNo:IDNo},
+                success:function(result){
+                    layer.msg("保存成功！");
+                    window.setTimeout("window.location=location.href",1000);
+                },
+                error:function(e){
+                    layer.msg("保存出错！");
+                    //window.setTimeout("window.location=location.href",1000);
+                }
+            });
+            layer.close(index);
         }
     });
-});
+
+    console.log();
+
+};
