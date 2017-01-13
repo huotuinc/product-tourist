12ed24e3a8d9247e6d79d5e9d0e2cc774b191915
Cai Jiang <luffy.ja@gmail.com>
2017-01-13 16:59:17 +0800

    make it run!

--- tourist-web/src/main/java/com/huotu/tourist/config/DispatcherServletInitializer.java
+++ tourist-web/src/main/java/com/huotu/tourist/config/DispatcherServletInitializer.java
@@ -4,7 +4,7 @@
  *
  * (c) Copyright Hangzhou Hot Technology Co., Ltd.
  * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
- * 2013-2016. All rights reserved.
+ * 2013-2017. All rights reserved.
  */
 
 package com.huotu.tourist.config;
@@ -18,7 +18,7 @@ public class DispatcherServletInitializer extends AbstractAnnotationConfigDispat
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class<?>[]{
-                MVCConfig.class
+                MVCConfig.class, RuntimeConfig.class
         };
     }
 
---
+++ tourist-web/src/main/java/com/huotu/tourist/config/RuntimeConfig.java
@@ -0,0 +1,23 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2017. All rights reserved.
+ */
+
+package com.huotu.tourist.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * 运行时配置
+ *
+ * @author CJ
+ */
+@Configuration
+@ImportResource("classpath:/datasource_runtime.xml")
+class RuntimeConfig {
+}
