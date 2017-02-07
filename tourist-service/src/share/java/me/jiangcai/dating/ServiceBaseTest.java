17a69bbaaf7c4319129e6fa51bf8b6eb978856a9
shiliting <861819089@qq.com>
2017-02-07 10:34:09 +0800

    重构WebTest和ServiceBaseTest，将WebTest里面的添加测试数据的方法提到ServiceBaseTest里面

--- tourist-service/src/share/java/me/jiangcai/dating/ServiceBaseTest.java
+++ tourist-service/src/share/java/me/jiangcai/dating/ServiceBaseTest.java
@@ -15,6 +15,7 @@ import com.huotu.tourist.entity.*;
 import com.huotu.tourist.repository.*;
 import com.huotu.tourist.service.ConnectMallService;
 import com.huotu.tourist.service.SettlementSheetService;
+import com.huotu.tourist.service.TouristGoodService;
 import me.jiangcai.lib.resource.service.ResourceService;
 import me.jiangcai.lib.test.SpringWebTest;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -42,7 +43,6 @@ import java.util.UUID;
 @ActiveProfiles({"test", "unit_test"})
 @ContextConfiguration(classes = {TestConfig.class, ServiceConfig.class})
 public abstract class ServiceBaseTest extends SpringWebTest {
-
     @Autowired
     protected ResourceService resourceService;
     @Autowired
@@ -68,12 +68,32 @@ public abstract class ServiceBaseTest extends SpringWebTest {
     @Autowired
     protected ActivityTypeRepository activityTypeRepository;
 
-
     @Autowired
     protected SettlementSheetService settlementSheetService;
 
     @Autowired
     protected SettlementSheetRepository settlementSheetRepository;
+
+    @Autowired
+    protected BannerRepository bannerRepository;
+
+    @Autowired
+    protected PurchaserPaymentRecordRepository purchaserPaymentRecordRepository;
+
+    @Autowired
+    protected PurchaserProductSettingRepository purchaserProductSettingRepository;
+
+    @Autowired
+    protected TouristGoodService touristGoodService;
+    /**
+     * 第几页字符串名称
+     */
+    protected String pageParameterName = "pageNo";
+    /**
+     * 每页几条字符串名称
+     */
+    protected String sizeParameterName = "pageSize";
+
 
 
     public String randomBankCard() {
@@ -275,7 +295,6 @@ public abstract class ServiceBaseTest extends SpringWebTest {
         return touristBuyerRepository.saveAndFlush(buyer);
     }
 
-
     protected TouristOrder createRandomTouristOrder(TouristGood good, TouristBuyer buyer) {
 
         TouristRoute touristRoute = new TouristRoute();
@@ -386,6 +405,240 @@ public abstract class ServiceBaseTest extends SpringWebTest {
 
     }
 
+    /**
+     * 创建一个随机的订单状态
+     *
+     * @return 订单状态
+     */
+    protected OrderStateEnum randomOrderStateEnum() {
+        int orderStateNo = random.nextInt(7);
+        switch (orderStateNo) {
+            case 0:
+                return OrderStateEnum.NotPay;
+            case 1:
+                return OrderStateEnum.PayFinish;
+            case 2:
+                return OrderStateEnum.NotFinish;
+            case 3:
+                return OrderStateEnum.Finish;
+            case 4:
+                return OrderStateEnum.Invalid;
+            case 5:
+                return OrderStateEnum.Refunds;
+            default:
+                return OrderStateEnum.RefundsFinish;
+        }
+    }
+
+//    protected boolean ListEqualsNotSort(List listExp,List listAct){
+//
+//    }
+
+    /**
+     * 创建一个随机的支付方式
+     *
+     * @return 支付方式
+     */
+    protected PayTypeEnum randomPayTypeEnum() {
+        int payTypeNo = random.nextInt(2);
+        switch (payTypeNo) {
+            case 0:
+                return PayTypeEnum.Alipay;
+            default:
+                return PayTypeEnum.WeixinPay;
+        }
+    }
+
+    /**
+     * 创建一个审查状态
+     *
+     * @return 审查状态
+     */
+    protected SettlementStateEnum randomSettlementStateEnum() {
+        int stateNo = random.nextInt(2);
+        switch (stateNo) {
+            case 0:
+                return SettlementStateEnum.NotChecking;
+            default:
+                return SettlementStateEnum.CheckFinish;
+        }
+    }
+
+    /**
+     * 创建一个随机的支付方式
+     *
+     * @return 支付方式
+     */
+    protected BuyerCheckStateEnum randomBuyerCheckState() {
+        int payTypeNo = random.nextInt(3);
+        switch (payTypeNo) {
+            case 0:
+                return BuyerCheckStateEnum.Checking;
+            case 1:
+                return BuyerCheckStateEnum.CheckFinish;
+            default:
+                return BuyerCheckStateEnum.Frozen;
+        }
+    }
+
+    /**
+     * 创建当前的时间，如果状态是未支付则返回null
+     *
+     * @param orderStateEnum 支付状态
+     * @return 当前时间
+     */
+    protected LocalDateTime randomLocalDateTime(OrderStateEnum orderStateEnum) {
+        if (!OrderStateEnum.NotPay.equals(orderStateEnum)) {
+            return LocalDateTime.now();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 创建一个测试线路订单
+     *
+     * @return 测试的线路订单
+     */
+    protected TouristOrder createTouristOrder(TouristGood good, TouristBuyer buyer, String orderNo
+            , OrderStateEnum orderState, LocalDateTime createTime, LocalDateTime payTime
+            , PayTypeEnum payType, String remark, SettlementSheet settlement) {
+        TouristOrder order = new TouristOrder();
+        order.setTouristGood(good == null ? createTouristGood(null, null, null, null, null) : good);
+        order.setTouristBuyer(buyer == null ? createTouristBuyer(null, null, null, null) : buyer);
+        order.setOrderNo(orderNo == null ? UUID.randomUUID().toString() : orderNo);
+        order.setCreateTime(createTime == null ? LocalDateTime.now() : createTime);
+        order.setOrderState(orderState == null ? randomOrderStateEnum() : orderState);
+        order.setPayTime(payTime == null ? LocalDateTime.now() : payTime);
+        order.setPayType(payType == null ? randomPayTypeEnum() : payType);
+        order.setRemarks(remark);
+        order.setSettlement(settlement);
+
+        return touristOrderRepository.saveAndFlush(order);
+    }
+
+    /**
+     * 创建一个结算单
+     *
+     * @param createTime       创建时间
+     * @param platformChecking 平台审核状态
+     * @param supplierName     供应商名称
+     * @return
+     */
+    protected SettlementSheet createSettlementSheet(LocalDateTime createTime, SettlementStateEnum platformChecking,
+                                                    String supplierName) {
+        SettlementSheet settlementSheet = new SettlementSheet();
+        settlementSheet.setCreateTime(createTime == null ? LocalDateTime.now() : createTime);
+        settlementSheet.setPlatformChecking(platformChecking == null ? randomSettlementStateEnum() : platformChecking);
+        return settlementSheetRepository.saveAndFlush(settlementSheet);
+    }
+
+    /**
+     * 创建一个线路商品
+     *
+     * @param name            线路名称
+     * @param activityType    活动类型
+     * @param touristType     线路类型
+     * @param checkState      线路审核状态
+     * @param touristSupplier 线路供应商
+     * @return 线路
+     */
+    protected TouristGood createTouristGood(String name, ActivityType activityType, TouristType touristType
+            , TouristCheckStateEnum checkState, TouristSupplier touristSupplier) {
+        TouristGood touristGood = new TouristGood();
+        touristGood.setTouristName(name == null ? UUID.randomUUID().toString() : name);
+        touristGood.setActivityType(activityType == null ? createActivityType(null) : activityType);
+        touristGood.setTouristType(touristType == null ? createTouristType(null) : touristType);
+        touristGood.setTouristCheckState(checkState == null ? randomTouristCheckStateEnum() : checkState);
+        touristGood.setTouristSupplier(touristSupplier == null ? createTouristSupplier(null) : touristSupplier);
+        touristGood.setRecommend(false);
+        return touristGoodRepository.saveAndFlush(touristGood);
+    }
+
+    /**
+     * 创建一个行程路线
+     *
+     * @param routeNo
+     * @param good
+     * @param fromDate
+     * @param toDate
+     * @param maxPeople
+     * @return
+     */
+    protected TouristRoute createTouristRoute(String routeNo, TouristGood good, LocalDateTime fromDate, LocalDateTime toDate,
+                                              int maxPeople) {
+        TouristRoute touristRoute = new TouristRoute();
+        touristRoute.setRouteNo(routeNo);
+        touristRoute.setGood(good);
+        touristRoute.setFromDate(fromDate);
+        touristRoute.setToDate(toDate);
+        touristRoute.setMaxPeople(maxPeople);
+        return touristRouteRepository.saveAndFlush(touristRoute);
+    }
+
+    /**
+     * 创建一个游客
+     *
+     * @param touristRoute
+     * @param order
+     * @return
+     */
+    protected Traveler createTraveler(TouristRoute touristRoute, TouristOrder order) {
+        Traveler traveler = new Traveler();
+        traveler.setOrder(order);
+        traveler.setRoute(touristRoute);
+        return travelerRepository.saveAndFlush(traveler);
+    }
+
+    /**
+     * 创建一个采购商
+     *
+     * @param buyerName       名称
+     * @param buyerDirector   负责人
+     * @param telPhone        电话
+     * @param buyerCheckState 审核状态
+     * @return
+     */
+    protected TouristBuyer createTouristBuyer(String buyerName, String buyerDirector, String telPhone
+            , BuyerCheckStateEnum buyerCheckState) {
+        TouristBuyer touristBuyer = new TouristBuyer();
+        touristBuyer.setCreateTime(LocalDateTime.now());
+        touristBuyer.setBuyerName(buyerName == null ? UUID.randomUUID().toString() : buyerName);
+        touristBuyer.setBuyerDirector(buyerDirector == null ? UUID.randomUUID().toString() : buyerDirector);
+//        touristBuyer.setBuyerId(UUID.randomUUID().toString().replace("-", ""));
+        touristBuyer.setTelPhone(telPhone == null ? "13012345678" : telPhone);
+        touristBuyer.setCheckState(buyerCheckState == null ? randomBuyerCheckState() : buyerCheckState);
+        touristBuyer.setCreateTime(LocalDateTime.now());
+        return touristBuyerRepository.saveAndFlush(touristBuyer);
+    }
+
+    /**
+     * 添加采购商支付记录
+     *
+     * @param touristBuyer 采购商
+     * @param payDate      支付时间
+     * @return
+     */
+    protected PurchaserPaymentRecord createPurchaserPaymentRecord(TouristBuyer touristBuyer, LocalDateTime payDate) {
+        PurchaserPaymentRecord purchaserPaymentRecord = new PurchaserPaymentRecord();
+        purchaserPaymentRecord.setTouristBuyer(touristBuyer == null ? createTouristBuyer(null, null, null, null) : touristBuyer);
+        purchaserPaymentRecord.setPayDate(payDate == null ? LocalDateTime.now() : payDate);
+        purchaserPaymentRecord.setCreateTime(LocalDateTime.now());
+        return purchaserPaymentRecordRepository.saveAndFlush(purchaserPaymentRecord);
+    }
+
+    /**
+     * 采购商产品设置
+     *
+     * @param name 采购商产品设置名称
+     * @return
+     */
+    protected PurchaserProductSetting createPurchaserProductSetting(String name) {
+        PurchaserProductSetting purchaserProductSetting = new PurchaserProductSetting();
+        purchaserProductSetting.setName(name == null ? UUID.randomUUID().toString().replace("-", "") : name);
+        purchaserProductSetting.setPrice(new BigDecimal(100));
+        return purchaserProductSettingRepository.saveAndFlush(purchaserProductSetting);
+    }
 
     public static class RandomComparator implements Comparator<Object> {
         static Random random = new Random();
@@ -395,9 +648,5 @@ public abstract class ServiceBaseTest extends SpringWebTest {
             return random.nextInt();
         }
     }
-
-//    protected boolean ListEqualsNotSort(List listExp,List listAct){
-//
-//    }
 
 }
